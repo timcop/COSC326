8e4a1e091509431695e53f8be59716c8648508e1
jordankettles <70495950+jordankettles@users.noreply.github.com>
2021-03-05 12:46:03 +1300

    adding more cases

--- syllables.py
+++ syllables.py
@@ -1,6 +1,6 @@
 import sys
 words_array = []
-vowels = ['a','e','i','o','u']
+vowels = ['a','e','i','o','u', 'y']
 for line in sys.stdin:
     line = line.strip()
     words_array.append(line)
@@ -9,13 +9,24 @@ for line in words_array:
     vowel_count = 0
     syllables = []
     cur_syllable = ""
-    for letter in line:
+    for i, letter in enumerate(line):
         cur_syllable += letter
         if letter in vowels:
             vowel_count+= 1
-        else:
-            if vowel_count > 0:
+            #check if end of line
+            if i == len(line)-1 and letter != 'e':
                 syllables.append(cur_syllable)
                 cur_syllable = ""
                 vowel_count = 0
+            elif #l e case
+        else:
+            if vowel_count > 0:
+                #ed case
+                if cur_syllable[-1:] == 'e':
+                    if letter == 'd' or letter == 's':
+                        #do something
+                else:
+                    syllables.append(cur_syllable)
+                    cur_syllable = ""
+                    vowel_count = 0
     print(line, len(syllables))
