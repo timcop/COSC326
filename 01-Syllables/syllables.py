8415b8ae4bcae767b3331019e3f235d52eb89b97
Magdeline <54191678+xsol05@users.noreply.github.com>
2021-03-05 13:21:09 +1300

    delete old syllables and move new syllables

--- 01-Syllables/syllables.py
+++ 01-Syllables/syllables.py
@@ -1,27 +1,38 @@
 import sys
 words_array = []
 vowels = ['a','e','i','o','u', 'y']
-for line in sys.stdin:
-    line = line.strip()
-    words_array.append(line)
-for line in words_array:
+for word in sys.stdin:
+    word = word.strip()
+    words_array.append(word)
+for word in words_array:
     syllable_count = 0
     vowel_count = 0
     syllables = []
     cur_syllable = ""
-    # length = line.length
-    for letter in line:
+    for i, letter in enumerate(word):
+        cur_syllable += letter
         if letter in vowels:
-            cur_syllable += letter
             vowel_count+= 1
-            if letter == line[len(line)-1] and letter != 'e':
+            #check if end of word
+            if (i == len(word)-1 and letter != 'e') or (i == len(word)-1 and letter == 'e' and cur_syllable[-2:-1] == 'l'):
                 syllables.append(cur_syllable)
                 cur_syllable = ""
+                vowel_count = 0
         else:
-            if cur_syllable[-1:] in vowels:
-                cur_syllable += letter
-                syllables.append(cur_syllable)
-                cur_syllable = ""
+            if vowel_count > 0:
+                #ed case
+                if i != len(word)-1:
+                    syllables.append(cur_syllable)
+                    cur_syllable = ""
+                    vowel_count = 0
+                elif letter not in ['d', 's']:
+                    syllables.append(cur_syllable)
+                    cur_syllable = ""
+                    vowel_count = 0
+                elif cur_syllable[-2:-1] != 'e' or (cur_syllable[-2:-1] == 'e' and vowel_count > 1):
+                    syllables.append(cur_syllable)
+                    cur_syllable = ""
+                    vowel_count = 0
 
 
-    print(syllables)
+    print(word, len(syllables))
