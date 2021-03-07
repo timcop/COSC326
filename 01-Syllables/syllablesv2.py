8f50e4b4ab2f6f0889d9655696089b6d9e39b74c
timcop <timothy.copland98@gmail.com>
2021-03-07 16:45:49 +1300

    Commented/cleaned up

--- 01-Syllables/syllablesv2.py
+++ 01-Syllables/syllablesv2.py
@@ -18,30 +18,34 @@ for word in words_array:
     # Iterate over word to 2nd last letter
     for i in range(0, len(word) - 1):
         letter = word[i]
-        if vowel_count == 0:
+        
+        if vowel_count == 0: # If no vowels are in the current syllable, add the letter to current syllable
             cur_syllable += letter
             if letter in vowels:
                 vowel_count+=1
-        elif vowel_count == 1:
+        else:
             if (letter in vowels):
-                if cur_syllable[-1] in vowels:
+                if cur_syllable[-1] in vowels: # Generally if we have 2 vowels in a row they are in the same syllable
                     cur_syllable += letter
-                else:
+                else: # Add current syllable to the syllables array then start a new syllable with current letter
                     syllables.append(cur_syllable)
                     cur_syllable = letter
                     vowel_count = 1
-            else:
+            else: # Add the letter to the current syllable if it isn't a vowel
                 cur_syllable += letter
  
+
+    # Here we handle a few cases for the end of the word
     letter = word[-1]
+    # Generally, a word ending in "Const, Const, 'y'" should be seperated into two syllables "Const, Const" & "'y"
     if (letter == 'y' and not(cur_syllable[-1] in vowels) and not(word[len(word) - 3] in vowels)):
         syllables.append(cur_syllable)
         syllables.append('y')
     else:
-        cur_syllable+=letter
-        if cur_syllable == 'ed':
+        cur_syllable+=letter 
+        if cur_syllable == 'ed': # Generally words ending in 'ed' will not be a new syllable
             syllables[-1] += cur_syllable
-        elif (cur_syllable == 'es' and not(syllables[-1][-1] in ['c', 's'])):
+        elif (cur_syllable == 'es' and not(syllables[-1][-1] in ['c', 's'])): # Words ending in 'es' but not 'ces' and 'ses' will not be a new syllable
             syllables[-1] += cur_syllable
         
         else:
