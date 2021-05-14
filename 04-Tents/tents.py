9d8e8a83499ad416a4b97bff8c460c5f87d1ecdd
jordankettles <70495950+jordankettles@users.noreply.github.com>
2021-05-14 12:15:31 +1200

    fixed input parsing

--- 04-Tents/tents.py
+++ 04-Tents/tents.py
@@ -10,39 +10,9 @@ tent = 1
 problem = []
 problems_all = []
 tree_list = []
-counts = []
-for line in sys.stdin:
-    if not line.isspace():
-        line = line.split()
 
-        if len(line) == 1: # Then trees and dot line
-            row = []
-            for character in line[0]:
-                if character == '.':
-                    row.append(empty)
-                elif character == 'T':
-                    row.append(tree)
-            tree_list.append(row)
-        else:
-            counts.append([int(i) for i in line])
-    else:
-        problem.append(tree_list)
-        problem.append(counts)
-        problems_all.append(problem)
-        tree_list = []
-        counts = []
-problem.append(tree_list)
-problem.append(counts)
-problems_all.append(problem)
-
-print(problems_all)
-for prob in problems_all:
-    tree_list = prob[0]
-    tree_array = np.array(prob[0])
-    counts = prob[1].copy()
-    row_counts = counts[0]
-    col_counts = counts[1]
-
+def solveProblem(tree_list, row_counts, col_counts):
+    tree_array = np.array(tree_list)
     # First stage, this removes potential locations for tents to go
     # by finding the 0's
     for row, num in enumerate(row_counts):
@@ -96,37 +66,63 @@ for prob in problems_all:
     total_tents = sum(row_counts)
     placed = 0
     print(tree_array)
-
-        while placed < total_tents:
-            print(placed)
-            # Perform operation on rows
-            ## MARK ALL SURROUNDING TILES WITH -1 AFTER PLACEMENT
-            for row, row_num in enumerate(row_counts):
-                # If available slots = num then place all tents
-                slot_count = 0
-                slot_index = []
-                for col, col_num in enumerate(col_counts):
-                    if tree_array[row][col] == 0:
-                        slot_count += 1
-                        slot_index.append(col)
-                if slot_count == row_num:
-                    for index in slot_index:
-                        tree_array[row][index] = 1
-                    placed += row_num
-            # Exact same for columns
+    while placed < total_tents:
+        print(placed)
+        # Perform operation on rows
+        ## MARK ALL SURROUNDING TILES WITH -1 AFTER PLACEMENT
+        for row, row_num in enumerate(row_counts):
+            # If available slots = num then place all tents
+            slot_count = 0
+            slot_index = []
             for col, col_num in enumerate(col_counts):
-                # If available slots = num then place all tents
-                slot_count = 0
-                slot_index = []
-                for row, row_num in enumerate(row_counts):
-                    if tree_array[row][col] == 0:
-                        slot_count += 1
-                        slot_index.append(row)
-                if slot_count == col_num:
-                    for index in slot_index:
-                        tree_array[index][col] = 1
-                    placed += col_num
+                if tree_array[row][col] == 0:
+                    slot_count += 1
+                    slot_index.append(col)
+            if slot_count == row_num:
+                for index in slot_index:
+                    tree_array[row][index] = 1
+                placed += row_num
+        # Exact same for columns
+        for col, col_num in enumerate(col_counts):
+            # If available slots = num then place all tents
+            slot_count = 0
+            slot_index = []
+            for row, row_num in enumerate(row_counts):
+                if tree_array[row][col] == 0:
+                    slot_count += 1
+                    slot_index.append(row)
+            if slot_count == col_num:
+                for index in slot_index:
+                    tree_array[index][col] = 1
+                placed += col_num
 
+if __name__ == "__main__":
+    second_row = False
+    for line in sys.stdin:
+        if not line.isspace():
+            line = line.split()
 
+            if len(line) == 1: # Then trees and dot line
+                row = []
+                for character in line[0]:
+                    if character == '.':
+                        row.append(empty)
+                    elif character == 'T':
+                        row.append(tree)
+                tree_list.append(row)
+            else:
+                if not second_row:
+                    problem.append(tree_list.copy())
+                    problem.append(list(int(j) for j in line))
+                    tree_list.clear()
+                    second_row = True
+                # print(problem)
+                elif second_row:
+                    problem.append(list(int(j) for j in line))
+                    problems_all.append(problem.copy())
+                    problem.clear()
+                    second_row = False
 
-    # print(tree_array)
+    for item in problems_all:
+        #solveProblem(item[0], item[1], item[2])
+        print(item)
