adc444d8f3be2bb60f70d1d316eecf6f68e67702
jordankettles <70495950+jordankettles@users.noreply.github.com>
2021-05-21 14:24:11 +1200

    Update Test.java

--- 07-For_Sale/Source/Test.java
+++ 07-For_Sale/Source/Test.java
@@ -20,7 +20,7 @@ public class Test {
        for(int i = 0; i < 1; i++) {
            players.add(new Player("R"+ ((char) ('A' + i)), new RandomStrategy()));
        }
-       players.add(new Player("Strat3", new Strat3()));
+       players.add(new Player("Strat5", new Strat5()));
        players.add(new Player("Strat2", new Strat2()));
        players.add(new Player("Strat1", new Strat1()));
         java.util.Collections.shuffle(players);
