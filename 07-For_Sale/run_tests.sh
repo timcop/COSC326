7bfaf12974db3c20ada91a88dbcc0d1e53053e2b
Ethan Fraser <ejpfraser@gmail.com>
2021-05-24 22:44:44 +1200

    updated run_tests.sh

--- 07-For_Sale/run_tests.sh
+++ 07-For_Sale/run_tests.sh
@@ -1,14 +1,18 @@
+#!/bin/bash
+
 echo Welcome, this will run forsale.Test
 
 # Set amount of times to run from user input
 read -p "Please enter how many times to run the program: " numToRun
 
+rm scores.txt
+
 javac -d . Source/*.java Source/strategies/*.java
 
 # Loop from 0 to numToRun
-for i in 0...$numToRun
+for i in $(seq 1 $numToRun)
 do
-    java forsale.Test > scores.txt
+    java forsale.Test >> scores.txt
 done
 
 exit 0
